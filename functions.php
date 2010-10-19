dd9078bf34d6b5bf1ffcfd0e397e8583a71da42a
Max Albrecht <1@eins78.com>
2010-10-19 19:00:03 +0200

    integrating "http://github.com/bueltge/different-type" to costumize it because add_meta_box makes me dizzy

--- functions.php
+++ functions.php
@@ -175,3 +175,68 @@ function m18_register_post_types()
   register_post_type('m18_protokoll',$m18_protokoll_args);
   register_post_type('m18_termin',$m18_termin_args);
 }
+
+/* 
+Adding meta_box
+*/
+
+/* Define the custom box */
+add_action('add_meta_boxes', 'm18_add_custom_box');
+
+/* Do something with the data entered */
+add_action('save_post', 'm18_save_postdata');
+
+/* Adds a box to the main column on the Post and Page edit screens */
+function m18_add_custom_box() {
+    add_meta_box( 'm18_sectionid', __( 'Protokolltext', 'm18_textdomain' ), 
+                'm18_inner_custom_box', 'm18_protokoll' );
+}
+
+/* Prints the box content */
+function m18_inner_custom_box() {
+
+  // Use nonce for verification
+  wp_nonce_field( plugin_basename(__FILE__), 'm18_noncename' );
+
+  // The actual fields for data entry
+  echo '<label for="m18_new_field">' . __("Description for this field", 'm18_textdomain' ) . '</label> ';
+  echo '<input type="text" id= "m18_new_field" name="m18_new_field" value="whatever" size="25" />';
+}
+
+/* When the post is saved, saves our custom data */
+function m18_save_postdata( $post_id ) {
+
+  // verify this came from the our screen and with proper authorization,
+  // because save_post can be triggered at other times
+
+  if ( !wp_verify_nonce( $_POST['m18_noncename'], plugin_basename(__FILE__) )) {
+    return $post_id;
+  }
+
+  // verify if this is an auto save routine. If it is our form has not been submitted, so we dont want
+  // to do anything
+  if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+    return $post_id;
+
+  
+  // Check permissions
+  if ( 'page' == $_POST['post_type'] ) {
+    if ( !current_user_can( 'edit_page', $post_id ) )
+      return $post_id;
+  } else {
+    if ( !current_user_can( 'edit_post', $post_id ) )
+      return $post_id;
+  }
+
+  // OK, we're authenticated: we need to find and save the data
+
+  $m18_data = $_POST['m18_new_field'];
+
+  // Do something with $m18_data 
+  // probably using add_post_meta(), update_post_meta(), or 
+  // a custom table (see Further Reading section below)
+
+   return $m18_data;
+}
+
+include 'different-type/different-type.php';
